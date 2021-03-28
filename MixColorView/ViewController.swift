24612124540b2c303434a5ab3c551fbd66528274
BulatKamalov1985 <Kamalovmarket@gmail.com>
2021-03-28 09:29:13 +0300

    Finish version without optimisation code

--- MixColorView/ViewController.swift
+++ MixColorView/ViewController.swift
@@ -26,44 +26,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         mixColorView.layer.cornerRadius = 20
         
-        
         redLabel.textColor = .red
-        greenLabel.textColor = .green
-        blueLabel.textColor = .blue
-        
         redColorSLider.minimumTrackTintColor = .red
-        greenColorSlider.minimumTrackTintColor = .green
-        blueColorSlider.minimumTrackTintColor = .blue
-        
         redColorSLider.value = 1
         redColorSLider.minimumValue = 0
         redColorSLider.maximumValue = 1
-        redVolueLabel.text = String(redColorSLider.value)
         
+        greenLabel.textColor = .green
+        greenColorSlider.minimumTrackTintColor = .green
         greenColorSlider.value = 1
         greenColorSlider.minimumValue = 0
         greenColorSlider.maximumValue = 1
-        greenVolueLabel.text = String(greenColorSlider.value)
         
+        blueLabel.textColor = .blue
+        blueColorSlider.minimumTrackTintColor = .blue
         blueColorSlider.value = 1
         blueColorSlider.minimumValue = 0
         blueColorSlider.maximumValue = 1
-        blueVolueLabel.text = String(blueColorSlider.value)
     }
     
-    
-    
-    @IBAction func redSliderAction() {
-        redVolueLabel.text = String( redColorSLider.value)
-        
-        let redSliderValue = CGFloat(redColorSLider.value)
-        let greenSliderValue = CGFloat(greenColorSlider.value)
-        let blueSliderValue = CGFloat(greenColorSlider.value)
-        
-        mixColorView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
-    }
-    @IBAction func greenSliderAction() {
-        greenVolueLabel.text = String(greenColorSlider.value)
+    @IBAction private func redSliderAction() {
+        redVolueLabel.text = String(format: "%.1f", redColorSLider.value)
         
         let redSliderValue = CGFloat(redColorSLider.value)
         let greenSliderValue = CGFloat(greenColorSlider.value)
@@ -71,8 +54,17 @@ class ViewController: UIViewController {
         
         mixColorView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
     }
-    @IBAction func blueSliderAction() {
-        blueVolueLabel.text = String(blueColorSlider.value)
+    @IBAction private func greenSliderAction() {
+        greenVolueLabel.text = String(format: "%.1f", greenColorSlider.value)
+        
+        let redSliderValue = CGFloat(redColorSLider.value)
+        let greenSliderValue = CGFloat(greenColorSlider.value)
+        let blueSliderValue = CGFloat(blueColorSlider.value)
+        
+        mixColorView.backgroundColor = UIColor(red: redSliderValue, green: greenSliderValue, blue: blueSliderValue, alpha: 1)
+    }
+    @IBAction private func blueSliderAction() {
+        blueVolueLabel.text = String(format: "%.1f", blueColorSlider.value)
        
         let blueSliderValue = CGFloat(blueColorSlider.value)
         let redSliderValue = CGFloat(redColorSLider.value)
